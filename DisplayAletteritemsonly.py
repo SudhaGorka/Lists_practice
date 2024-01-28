f3f6b3af645b6a0e70c7bcb68a8fd98f8b489983
Sudha Gorka <100294985+SudhaGorka@users.noreply.github.com>
2024-01-28 22:49:15 +0530

    Add files via upload

---
+++ DisplayAletteritemsonly.py
@@ -0,0 +1,15 @@
+#Write a Program , where list ‘L’ is passed as an argument. Display those strings which are start with ‘A’.
+# L = [‘RINKU’,’AUSHIM’,’VIJAYA’,’AKHTAR’,’LEENA’,’TARUN’,’AMAR’]
+
+L = ['RINKU','aUSHIM','VIJAYA','aKHTAR','LEENA','TARUN','aMAR']
+
+for i in L:
+    if i[0] in ('a'):
+        print(i)
+
+
+        
+    
+
+
+
